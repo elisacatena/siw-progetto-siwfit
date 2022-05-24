4ec29fe47df18333e1131ce482068166cd00e84b
Elisa Catena <elisacatena00@gmail.com>
2022-05-24 21:03:03 +0200

    Modificati i controller

--- siw-progettoSiwFit/src/main/java/com/example/demo/controller/CorsoController.java
+++ siw-progettoSiwFit/src/main/java/com/example/demo/controller/CorsoController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import com.example.demo.model.Corso;
 import com.example.demo.service.CorsoService;
+import com.example.demo.service.UserService;
 import com.example.demo.validator.CorsoValidator;
 
 @Controller
@@ -21,6 +22,8 @@ public class CorsoController {
 	@Autowired
 	private CorsoValidator corsoValidator;
 	
+	@Autowired
+	private UserService userService;
 	
 //	@GetMapping("/corsi")
 //	public String getCorsi(Model model) {
@@ -29,11 +32,24 @@ public class CorsoController {
 //		return "corsi.html";
 //	}
 
-	@GetMapping("/corso/{id}")
+	/* id è del corso.
+	 * Il metodo resituisce un corso tramite il suo id.
+	 */
+	@GetMapping("/user/corso/{id}")
 	public String getCorso(@PathVariable("id")Long id, Model model) {
 		Corso corso = corsoService.findById(id);
 		model.addAttribute("corso", corso);
 		return "corso.html";
 	}
+	
+	@GetMapping("/user/prenota/{id}")
+	public String getCorsiPrenotati( @PathVariable("id") Long id, Model model) {
+		List<Corso> prenotati = this.userService.findAllCorsiPrenotati("u1");
+		prenotati.add(this.corsoService.findById(id));
+		model.addAttribute("prenotati", prenotati);
+		return "homeUser.html";
+	}
+	
+
 
 }
